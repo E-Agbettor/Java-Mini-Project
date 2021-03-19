731c7c91f7b74d4d9fce478718e8a4c2a38c9272
unknown <elliot.agbettor@amalitech.org>
2021-03-19 15:10:34 +0000

    update on mave dependencies

---
+++ src/test/java/test/classes/BankTest.java
@@ -0,0 +1,28 @@
+package test.classes;
+
+import static org.junit.Assert.*;
+
+public class BankTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void addBranch() {
+    }
+
+    @org.junit.Test
+    public void addCustomer() {
+    }
+
+    @org.junit.Test
+    public void addCustomerTransaction() {
+    }
+
+    @org.junit.Test
+    public void listCustomers() {
+    }
+
+
+}
